c96197426c08130ea6fa3ebdba7a0eed8c3c61c2
DESKTOP-BQOH35F\arashicage <arashicage@yeah.net>
2018-04-18 00:06:29 +0800

    fix chmod prob,and modify config default dir

--- twemproxy-0.4.1/start.sh
+++ twemproxy-0.4.1/start.sh
@@ -6,7 +6,7 @@ STATS_ADDR=${STATS_ADDR:-0.0.0.0}
 # Stats Generation Interval in Miliseconds
 STATS_INTERVAL=${STATS_INTERVAL:-30000}
 # Configuration File Path
-CONF_FILE=${CONF_FILE:-/scripts/nutcracker.yml}
+CONF_FILE=${CONF_FILE:-/usr/local/etc/nutcracker/nutcracker.yml}
 # Verbose Level
 VERBOSE=${VERBOSE:-5}
 /usr/sbin/nutcracker --conf-file=$CONF_FILE --stats-port=$STATS_PORT --stats-addr=$STATS_ADDR --stats-interval=$STATS_INTERVAL --verbose=$VERBOSE
